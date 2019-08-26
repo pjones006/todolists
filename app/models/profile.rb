6dd048cd8d489f82f0db8a0edb68566e32c85fa0
Paul Jones <pjones006@att.net>
2019-08-26 15:35:08 -0700

    Finished and passed week2 assignment

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,24 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+  validate :presence_of_first_or_last_name 
+  validate :males_no_sue
+  validates :gender, inclusion: %w(male female Male Female)
+
+  def self.get_all_profiles(start_year, end_year) 
+    where(birth_year: start_year..end_year).order(:birth_year ) 
+  end
+
+  def presence_of_first_or_last_name
+    if (first_name.blank? && last_name.blank?)
+      errors[:base] << ("Specify a first or a last.")
+    end
+  end
+
+  def males_no_sue
+    if (gender == "male" && first_name == "Sue") 
+        puts "true"
+        errors[:base] << ("Sue is not the name of a male.")
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
-    has_one :profile
-    has_many :todo_lists , dependent: :delete_all
-    has_many :todo_items , through: :todo_lists , source: :todo_items
+    
+    has_one :profile , dependent: :destroy
+    has_many :todo_lists , dependent: :delete_all ,  dependent: :destroy
+    has_many :todo_items , through: :todo_lists , source: :todo_items , dependent: :destroy
+
+    validates :username , presence: true
+# 
+# User.all.each { {u} puts u.todo_items.completed }
+    def get_completed_count
+       todo_items.where(completed: true).count
+     end
 end
